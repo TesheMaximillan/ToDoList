0ea9f79543ef406c46b70ac26cc304f2cb0418ab
Teshome Kurabachew <dear.teshe@gmail.com>
2022-03-23 03:34:10 +0300

    Add Book list from the array object

--- src/index.js
+++ src/index.js
@@ -1,11 +1,51 @@
 import _ from 'lodash';
 import './main.scss';
 
-// function component() {
-//   const element = document.createElement('div');
+const listContainer = document.querySelector('.list-container__items');
 
-//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-//   return element;
-// }
+const tasks = [
+  {
+    description: "Standup Team",
+    completed: false,
+    index: 3
+  },
+  {
+    description: "Morning Session",
+    completed: true,
+    index: 1
+  },
+  {
+    description: "Coding Partner",
+    completed: false,
+    index: 2
+  },
+]
 
-// document.body.appendChild(component());
+tasks.sort((a, b) => {
+  return a.index - b.index
+})
+
+tasks.forEach(e => {
+  const li = document.createElement('li');
+  const input = document.createElement('input');
+  const span = document.createElement('span');
+  input.type = 'checkbox';
+  li.className = 'list-container__items--item';
+  span.className = '';
+  span.textContent = e.description;
+  li.append(input, span);
+  listContainer.appendChild(li);
+})
+
+const checkbox = document.querySelectorAll('input[type=checkbox]');
+
+const checkMark = input => {
+  input.addEventListener('change', () => {
+    if (input.checked) {
+      input.nextElementSibling.classList.add('checked');
+    }else input.nextElementSibling.classList.remove('checked');;
+  })
+}
+
+checkbox.forEach(e => checkMark(e))
+
