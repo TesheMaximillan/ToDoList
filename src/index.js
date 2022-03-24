88a5ae7d33c062e90c91ea4971b0f9b724694c4d
Teshome Kurabachew <dear.teshe@gmail.com>
2022-03-24 22:47:45 +0300

    Add list functionality

--- src/index.js
+++ src/index.js
@@ -1,19 +1,61 @@
 import './main.scss';
 import './asset/icons/sprite.svg';
-import { addTask } from './modules/controlList';
+
+import {
+  addTask, removeCompletedTask, removeTask, status, updateTask,
+} from './modules/controlList';
 
 const form = document.querySelector('.list-container__list-content');
+const clearButton = document.querySelector('.list-container__remove-all');
 
 addTask(form);
 
 const checkbox = document.querySelectorAll('input[type=checkbox]');
+const taskName = document.querySelectorAll('.task-name');
+const deleteList = document.querySelectorAll('.bar');
 
-const checkMark = (input) => {
-  input.addEventListener('change', () => {
-    if (input.checked) {
-      input.nextElementSibling.classList.add('checked');
-    } else input.nextElementSibling.classList.remove('checked');
+const checkMark = (i) => {
+  checkbox[i].addEventListener('change', () => {
+    if (checkbox[i].checked) {
+      checkbox[i].classList.add('input-after');
+      checkbox[i].nextElementSibling.classList.add('checked');
+      status(i, true);
+    } else {
+      checkbox[i].classList.remove('input-after');
+      checkbox[i].nextElementSibling.classList.remove('checked');
+      status(i, false);
+    }
   });
 };
 
-checkbox.forEach((e) => checkMark(e));
+const removeHighlight = () => {
+  taskName.forEach((e) => {
+    e.parentElement.classList.remove('list-highlight');
+    e.nextElementSibling.classList.remove('icon-delete');
+  });
+};
+
+clearButton.addEventListener('click', () => {
+  removeCompletedTask();
+  window.location.reload();
+});
+
+const selectTask = (i) => {
+  taskName[i].addEventListener('click', () => {
+    removeHighlight();
+    taskName[i].parentElement.classList.add('list-highlight');
+    taskName[i].nextElementSibling.classList.add('icon-delete');
+
+    deleteList[i].addEventListener('click', () => {
+      removeTask(i);
+      window.location.reload();
+    });
+
+    taskName[i].addEventListener('input', () => {
+      updateTask(i, taskName[i].value);
+    });
+  });
+};
+
+for (let i = 0; i < checkbox.length; i += 1) { checkMark(i); }
+for (let i = 0; i < taskName.length; i += 1) { selectTask(i); }
--- src/modules/controlList.js
+++ src/modules/controlList.js
@@ -1,4 +1,10 @@
 const listContainer = document.querySelector('.list-container__items');
+const notification = document.querySelector('.notification');
+const roration = document.querySelector('.fa-arrows-rotate');
+
+setTimeout(() => {
+  roration.classList.remove('spinner');
+}, 2000);
 
 let tasks = [];
 
@@ -16,20 +22,28 @@ const displayTask = () => {
   tasks.forEach((e) => {
     const li = document.createElement('li');
     const input = document.createElement('input');
-    const span = document.createElement('span');
+    const span = document.createElement('input');
     const i = document.createElement('i');
-    i.classList.add('icon', 'fa-solid', 'fa-ellipsis-vertical');
+    i.classList.add('icon', 'fa-solid', 'fa-ellipsis-vertical', 'bar');
 
     input.type = 'checkbox';
     li.className = 'list-container__items--item';
     span.className = 'task-name';
-    span.textContent = e.description;
+    span.value = e.description;
     li.append(input, span, i);
     listContainer.appendChild(li);
   });
+  roration.classList.add('spinner');
+  notification.textContent = tasks.length;
 };
 
 displayTask();
+
+const updateIndex = () => {
+  for (let i = 0; i < tasks.length; i += 1) {
+    tasks[i].index = i;
+  }
+};
 
 const addTask = (form) => {
   form.addEventListener('submit', (event) => {
@@ -43,4 +57,28 @@ const addTask = (form) => {
   });
 };
 
-export { addTask };
+const removeTask = (index) => {
+  tasks.splice(index, 1);
+  updateIndex();
+  window.localStorage.setItem('listItem', JSON.stringify(tasks));
+};
+
+const removeCompletedTask = () => {
+  for (let i = 0; i < tasks.length; i += 1) {
+    if (tasks[i].completed === true) removeTask(i);
+  }
+  updateIndex();
+};
+
+const updateTask = (index, value) => {
+  tasks[index].description = value;
+  window.localStorage.setItem('listItem', JSON.stringify(tasks));
+};
+
+const status = (index, type) => {
+  tasks[index].completed = type;
+};
+
+export {
+  addTask, removeTask, updateTask, status, removeCompletedTask,
+};
